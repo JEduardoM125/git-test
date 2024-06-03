dd85103f67965e963a2ea6b71ef2c38f17579d6d
JEduardoM125 <jm528485@gmail.com>
2024-06-02 21:23:05 -0600

    Ahora el hello world es hello CostaRica

--- App/App1/obj/Debug/net8.0/App1.AssemblyInfo.cs
+++ App/App1/obj/Debug/net8.0/App1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a27c21f374930104771457cc272f1aca826a1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
